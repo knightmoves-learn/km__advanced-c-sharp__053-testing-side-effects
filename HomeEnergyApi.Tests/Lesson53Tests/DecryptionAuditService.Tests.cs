449c98b8245c3e515baa4724c142fd76ff51d04a
Isaac <isaac@knightmoves.org>
2025-11-14 15:17:19 -0600

    test

--- HomeEnergyApi.Tests/Lesson53Tests/DecryptionAuditService.Tests.cs
+++ HomeEnergyApi.Tests/Lesson53Tests/DecryptionAuditService.Tests.cs
@@ -1,2 +1,24 @@
 using HomeEnergyApi.Services;
 
+public class DecryptionAuditServiceTest
+{
+    private StubLogger<DecryptionAuditService> stubLogger;
+    private DecryptionAuditService service;
+
+    public DecryptionAuditServiceTest()
+    {
+        stubLogger = new StubLogger<DecryptionAuditService>();
+        service = new DecryptionAuditService(stubLogger);
+    }
+
+    [Fact]
+    public void ShouldLog_WhenValueDecrypted()
+    {
+        string testCipher = "testCipher";
+        string plainText = "testText";
+
+        service.OnValueDecrypted(testCipher, plainText);
+        
+        Assert.Single(stubLogger.LoggedInfoMessages);
+    }
+}
--- HomeEnergyApi.Tests/Stubs/StubLogger.cs
+++ HomeEnergyApi.Tests/Stubs/StubLogger.cs
@@ -1,2 +1,23 @@
 using Microsoft.Extensions.Logging;
 
+public class StubLogger<T> : ILogger<T>
+{
+    public List<string> LoggedInfoMessages = new List<string>();
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        if (logLevel == LogLevel.Information)
+        {
+            LoggedInfoMessages.Add(formatter(state, exception));
+        }
+    }
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        throw new NotImplementedException();
+    }
+
+}
